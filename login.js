3656e33f92e1f5f1e1802856e7725236e12352ac
sax <66324759+saxnbt@users.noreply.github.com>
2021-02-10 16:51:33 +0000

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
 const wloc = window.location.pathname;
 const srdir = wloc.substring(0, wloc.lastIndexOf("/")+1);
 // Not actually login js but this will hopefully fix the issue.
+
+function failboot(){
+  console.log("The OS ran into a problem, report this issue at @ https://github.com/saxnbt/SaxOS/issues/new")
+}
